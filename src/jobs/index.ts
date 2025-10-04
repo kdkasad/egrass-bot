0418d8a2eec60caa92a1cbb2dfa9c4d77fe1adde
Kian Kasad <kian@kasad.com>
2025-10-03 21:40:42 -0400

    feat: warn Alex beforehand when no problems are selected for tomorrow

--- src/jobs/index.ts
+++ src/jobs/index.ts
@@ -1,9 +1,10 @@
 import type { Client } from "discord.js";
 
 import * as announce from "./announce";
+import * as warnAlex from "./warn-alex";
 
 export interface Job {
 	createJob(client: Client<true>): void;
 }
 
-export const jobs: Job[] = [announce];
+export const jobs: Job[] = [announce, warnAlex];
---
+++ src/jobs/warn-alex.ts
@@ -0,0 +1,25 @@
+import cron from "node-cron";
+import { getProblemsForTomorrow } from "../db";
+import { channelMention, type Client } from "discord.js";
+import { Channels, Users } from "../consts";
+
+export function createJob(client: Client<true>) {
+	cron.schedule("0 22 * * *", async () => {
+		try {
+			await execute(client);
+		} catch (error) {
+			console.error("Error running warn-alex job", error);
+		}
+	});
+}
+
+export async function execute(client: Client<true>) {
+	const problems = getProblemsForTomorrow();
+
+	if (problems.length > 0) return;
+
+	await client.users.send(Users.Alex, {
+		content: `Hey Alex, Mr. Toucher here!
+Just letting you know that there are currently no ${channelMention(Channels.Neetcode)} problems selected for tomorrow.`,
+	});
+}
