2a77017440bdcdf406ade3f633e09cab68e5515e
Kian Kasad <kian@kasad.com>
2026-01-20 22:19:49 -0500

    tweak: 🥀

--- src/events/messages/bruh.ts
+++ src/events/messages/bruh.ts
@@ -10,11 +10,15 @@ export function register(client: Client<true>) {
 async function handleMessage(message: Message) {
 	if (
 		!message.author.bot &&
-		message.content.match(/(?:\b6+\b.*\b7+\b)|(?:\b6+7+\b)/)
+		message.content.match(
+			/(?:\b(?:6+|six)\b.*\b(?:7+|seven)\b)|(?:\b6+7+\b)/i,
+		)
 	) {
 		await message.react("🥀");
 		await message.reply(
-			"OMG HAHA SO FUNNY SIX AND SEVEN ARE CONSECUTIVE DIGITS 🤯",
+			Math.random() < 0.01
+				? "https://tenor.com/view/bee-movie-layton-t-montgomery-monty-montgomery-67-6-7-gif-9758470031245276788"
+				: "OMG HAHA SO FUNNY SIX AND SEVEN ARE CONSECUTIVE DIGITS 🤯",
 		);
 	}
 }
