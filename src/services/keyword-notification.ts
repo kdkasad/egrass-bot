5dbaa2e8a8e895b01f1e94e5f9303cb0ed4b5328
Kian Kasad <kian@kasad.com>
2026-04-29 23:19:23 -0400

    feat: general notifications
    Closes #12

--- src/services/keyword-notification.ts
+++ src/services/keyword-notification.ts
@@ -14,8 +14,30 @@ import {
 import { Guilds, Users } from "../consts";
 import { traced } from "../utils/tracing";
 
+enum NotificationType {
+	DM,
+	Ping,
+}
+
+interface Trigger {
+	pattern: RegExp;
+	user: Users;
+	type: NotificationType;
+}
+
 export class KeywordNotificationService extends Feature {
-	static #PATTERN = /\b(kian|kasad)\b/i;
+	static readonly Triggers: Trigger[] = [
+		{
+			pattern: /\b(kian|kasad)\b/i,
+			user: Users.Kian,
+			type: NotificationType.DM,
+		},
+		{
+			pattern: /\b(discord\s*mod|cailey)\b/i,
+			user: Users.Cailey,
+			type: NotificationType.Ping,
+		},
+	];
 
 	#discord: DiscordService;
 
@@ -33,43 +55,42 @@ export class KeywordNotificationService extends Feature {
 		}
 	}
 
-	#test(message: Message): boolean {
-		if (
-			message.author.bot ||
-			message.author.id === Users.Kian ||
-			message.guildId !== Guilds.Egrass
-		) {
-			return false;
+	#getUsersToNotify(message: Message): Map<Users, Set<NotificationType>> {
+		if (message.author.bot || message.guildId !== Guilds.Egrass) {
+			return new Map();
 		}
-		if (KeywordNotificationService.#PATTERN.test(message.content)) {
-			return true;
-		} else if (
-			message.reference?.type === MessageReferenceType.Forward &&
-			message.reference.messageId &&
-			message.messageSnapshots.has(message.reference.messageId)
-		) {
-			const snapshot = message.messageSnapshots.get(message.reference.messageId)!;
-			return KeywordNotificationService.#PATTERN.test(snapshot.content);
+
+		const result = new Map<Users, Set<NotificationType>>();
+		const getOrInsert = <K, V>(map: Map<K, V>, key: K, val: V) => {
+			if (!map.has(key)) {
+				map.set(key, val);
+			}
+			return map.get(key)!;
+		};
+
+		for (const { pattern, user, type } of KeywordNotificationService.Triggers) {
+			// Don't notify users about their own messages
+			if (message.author.id === user) continue;
+
+			// Check content
+			if (pattern.test(message.content)) {
+				getOrInsert(result, user, new Set()).add(type);
+			}
+			// Check forwarded message content
+			else if (message.reference?.type === MessageReferenceType.Forward) {
+				const snapshot = message.messageSnapshots.get(message.reference.messageId!)!;
+				if (pattern.test(snapshot.content)) {
+					getOrInsert(result, user, new Set()).add(type);
+				}
+			}
 		}
-		return false;
+
+		return result;
 	}
 
 	@traced("event.handler")
 	async #handleNewMessage(message: OmitPartialGroupDMChannel<Message>) {
-		if (this.#test(message)) {
-			await this.#discord.sendDM(
-				Users.Kian,
-				`${userMention(message.author.id)} mentioned you in ${channelLink(message.channel.id, message.guildId!)}`,
-			);
-			await this.#discord.sendDM(Users.Kian, {
-				forward: { message },
-			});
-			Sentry.logger.info("Keyword notification sent", {
-				"message.id": message.id,
-				"user.id": message.author.id,
-				"destination.user.id": Users.Kian,
-			});
-		}
+		return this.#notifyUsers(this.#getUsersToNotify(message), message);
 	}
 
 	@traced("event.handler")
@@ -80,8 +101,49 @@ export class KeywordNotificationService extends Feature {
 		if (oldMessage.partial) {
 			oldMessage = await oldMessage.fetch();
 		}
-		if (!this.#test(oldMessage)) {
+
+		if (!this.#getUsersToNotify(oldMessage)) {
 			await this.#handleNewMessage(newMessage);
 		}
+	}
+
+	@traced()
+	async #notifyUsers(
+		users: Map<Users, Set<NotificationType>>,
+		message: OmitPartialGroupDMChannel<Message>,
+	) {
+		const notifications = users
+			.entries()
+			.flatMap(([user, types]) =>
+				types.values().map<[Users, NotificationType]>((type) => [user, type]),
+			);
+		const handlers: Record<NotificationType, (user: Users) => Promise<void>> = {
+			[NotificationType.DM]: async (user) => {
+				await this.#discord.sendDM(
+					user,
+					`${userMention(message.author.id)} mentioned you in ${channelLink(message.channel.id, message.guildId!)}`,
+				);
+				await this.#discord.sendDM(user, {
+					forward: { message },
+				});
+			},
+			[NotificationType.Ping]: async (user) => {
+				await message.reply({
+					content: `Looks like someone mentioned you, ${userMention(user)}`,
+					allowedMentions: { users: [user] },
+				});
+			},
+		};
+		await Promise.all(
+			notifications.map(async ([user, type]) => {
+				await handlers[type](user);
+				Sentry.logger.info("Keyword notification sent", {
+					"message.id": message.id,
+					"user.id": message.author.id,
+					"destination.user.id": user,
+					"notification.type": NotificationType[type],
+				});
+			}),
+		);
 	}
 }
