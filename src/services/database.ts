7bf7c0e25c370d51baf6014336769101c821e933
Kian Kasad <kian@kasad.com>
2026-05-01 10:59:39 -0400

    perf: enable database optimization

--- src/services/database.ts
+++ src/services/database.ts
@@ -8,6 +8,7 @@ import { traced } from "../utils/tracing";
 import * as schema from "../db/schema";
 import type { EnvService } from "./env";
 import { parseDuration } from "../utils/time";
+import type { CronService } from "./cron";
 
 export type AppDB = BunSQLiteDatabase<typeof schema>;
 
@@ -26,29 +27,35 @@ export class DatabaseService extends Service {
 		Sentry.logger.info(`${this._name} created`);
 	}
 
-	static async new(env: EnvService): Promise<DatabaseService> {
-		return Sentry.startSpan({ name: "DatabaseService.new", op: "function" }, async () => {
-			const rwConn = new Database(env.vars.DATABASE_FILE, {
-				strict: true,
-				create: true,
-			});
-			rwConn.run("PRAGMA journal_mode = WAL");
-			rwConn.run(`PRAGMA busy_timeout = ${DB_BUSY_TIMEOUT_MS}`);
-			rwConn.run("PRAGMA foreign_keys = ON");
-			const db = drizzle(rwConn, {
-				schema,
-				logger: {
-					logQuery(sql, params) {
-						Sentry.logger.debug("SQL query", { sql, params });
-					},
-				},
-			});
-			migrate(db, {
-				migrationsFolder: Bun.fileURLToPath(new URL("../../drizzle", import.meta.url)),
-			});
-			Sentry.logger.info("Database initialization complete");
-			return new DatabaseService(db, rwConn);
+	@traced()
+	static async new(env: EnvService, cron: CronService): Promise<DatabaseService> {
+		const rwConn = new Database(env.vars.DATABASE_FILE, {
+			strict: true,
+			create: true,
 		});
+		rwConn.run("PRAGMA journal_mode = WAL");
+		rwConn.run(`PRAGMA busy_timeout = ${DB_BUSY_TIMEOUT_MS}`);
+		rwConn.run("PRAGMA foreign_keys = ON");
+
+		// Recommended by <https://sqlite.org/lang_analyze.html>.
+		rwConn.run("PRAGMA optimize=0x10002");
+		cron.createJob("analyze database", "0 4 * * *", async () => {
+			rwConn.run("PRAGMA optimize");
+		});
+
+		const db = drizzle(rwConn, {
+			schema,
+			logger: {
+				logQuery(sql, params) {
+					Sentry.logger.debug("SQL query", { sql, params });
+				},
+			},
+		});
+		migrate(db, {
+			migrationsFolder: Bun.fileURLToPath(new URL("../../drizzle", import.meta.url)),
+		});
+		Sentry.logger.info("Database initialization complete");
+		return new DatabaseService(db, rwConn);
 	}
 
 	async query<T>(
@@ -62,6 +69,7 @@ export class DatabaseService extends Service {
 
 	@traced()
 	async stop(): Promise<void> {
+		this.rwConn.run("PRAGMA optimize");
 		this.rwConn.close();
 		Sentry.logger.info("Database connections closed");
 	}
