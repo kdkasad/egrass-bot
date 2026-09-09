3ddf3cc871a1e42c5d7226042f48be224ca9f5dd
shrungpatel <shrungpatel@yahoo.in>
2026-09-09 16:14:26 -0400

    removed triple_T user

--- src/consts.ts
+++ src/consts.ts
@@ -4,13 +4,13 @@ export enum Users {
 	Atharva = "1007790710042394624",
 	Sophia = "592551894337978369",
 	Cailey = "278316954585137152",
-	Triple_T = "452231251810713600",
 }
 
 export enum Roles {
-	Mute = "1463375447927689298",
-	NighttimeMute = "",
-	NighttimeRestricted = "",
+  Mute = "1463375447927689298",
+  ProblemSetter = "1528409413818519593",
+  NighttimeMute = "",
+  NighttimeRestricted = "",
 }
 
 export enum Channels {
